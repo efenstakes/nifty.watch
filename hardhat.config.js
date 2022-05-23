a32404bd7cab494c7982669960d7cc07a7032fda
Efen Stakes <efenstakes101@gmail.com>
2022-05-23 12:57:32 +0300

    update config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +15,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+// console.log("ROPSTEN_URL ", process.env.ROPSTEN_URL)
+// console.log("PRIVATE_KEY ", process.env.PRIVATE_KEY)
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -17,5 +25,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.11",
+  networks: {
+    // the original one that comes with hardhat
+    // ropsten: {
+    //   url: process.env.ROPSTEN_URL || "",
+    //   accounts:
+    //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.ROPSTEN_PROJECT_ID}`,
+      accounts: { mnemonic: process.env.MNEMONIC }
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
