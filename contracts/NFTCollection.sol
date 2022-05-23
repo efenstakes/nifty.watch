32f7d8867768b5f5a2df39fa4a9b3ae38677f898
Efen Stakes <efenstakes101@gmail.com>
2022-05-23 12:40:12 +0300

    create base for the collection contract

---
+++ contracts/NFTCollection.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+
+
+/// @title NFTCollection
+/// @author efenstakes
+/// @notice Manages NFT Collections 
+/// @dev this contract has logic to store, retrieve and manage NFT market
+contract NFTCollection is Ownable {
+
+
+
+}
